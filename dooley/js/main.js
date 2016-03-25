40a8d0dc349a082129a0106d4734bf189b160a82
Eddie Bowen <eddie@cosmo7.com>
2016-03-25 10:56:22 -0400

    add dooley, agile section

---
+++ dooley/js/main.js
@@ -0,0 +1,65 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    })
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Fit Text Plugin for Main Header
+    $("h1").fitText(
+        1.2, {
+            minFontSize: '35px',
+            maxFontSize: '200px'
+        }
+    );
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+    // Initialize WOW.js Scrolling Animations
+    new WOW().init();
+
+})(jQuery); // End of use strict
+
+
+$(document).ready(function() {
+
+    $("#typing").typed({
+        strings: ["Victorian Village", "Short North", "Italian Village", "Harrison West", "German Village", "Downtown Columbus"],
+        showCursor: false,
+        typeSpeed: 0
+    });
+
+    /*
+    function initMap() {
+        // Create a map object and specify the DOM element for display.
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: -34.397, lng: 150.644 },
+            scrollwheel: false,
+            zoom: 8
+        });
+    }
+
+    initMap();
+    */
+});
